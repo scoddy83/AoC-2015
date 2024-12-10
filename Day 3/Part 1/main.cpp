498d99deae685c79f4ff4d9cdc785cbab42faba0
neil scott soutar <soutar.n@gmail.com>
2024-12-10 22:40:44 +0100

    Day 3.1

--- Day 3/Part 1/main.cpp
+++ Day 3/Part 1/main.cpp
@@ -6,28 +6,40 @@
 struct point {
   int x;
   int y;
+  int parcel;
 };
 
 point move(char direction) {
-  point p = {0, 0};
+  point p = {0, 0, 0};
   switch (direction)
   {
   case '^':
-    return p = {0,1};
+    return p = {0,1,0};
   case '>':
-    return p = {1,0};
+    return p = {1,0,0};
   case 'v':
-    return p = {0,-1};
+    return p = {0,-1,0};
   case '<':
-    return p = {-1,0};
+    return p = {-1,0,0};
   }
   return p;
+}
+
+bool addParcel(std::vector<point> path, point const *p) {
+  for(std::size_t i = 0; i < path.size(); i++) {
+    //std::cout << "move-i: " << i << "; x : y = " << path[i].x << " : " << path[i].y << std::endl;
+    if(p->x == path[i].x && p->y == path[i].y) {
+      path[i].parcel++;
+      return true;
+    }
+  }
+  return false;
 }
 
 int main() {
   int sum = 0;
   std::string str;
-  point p = {0, 0};
+  point p = {0, 0, 1};
   std::vector<point> path;
   int i = 0;
 
@@ -36,21 +48,20 @@ int main() {
 
   // Start Point
   path.push_back(p);
-  std::cout << "move: " << i << "; x: " << path[i].x << "; y: " << path[i].y << std::endl;
   // Use a while loop together with the getline() function to read the file line by line
   while (std::getline(MyReadFile, str)) {
     // Output the text from the file
     try {
       while(str[i]) {
-        // Get Current Position
-        p.x = path[i].x;
-        p.y = path[i].y;
         // Set New Position
         p.x += move(str[i]).x;
         p.y += move(str[i]).y;
         
-        path.push_back(p);
-        std::cout << "move: " << i << "; x: " << path[i+1].x << "; y: " << path[i+1].y << std::endl;
+        //std::cout << "move: " << i << "; x : y = " << p.x << " : " << p.y << std::endl;
+        if(!addParcel(path, &p)) {
+          path.push_back(p);
+        }
+        //std::cout << "move: " << i << "; x : y = " << p.x << " : " << p.y << std::endl;
         i++;
       }
     } catch (const std::invalid_argument&) {
@@ -60,6 +71,12 @@ int main() {
 
   // Close the file
   MyReadFile.close();
+
+  for(std::size_t i = 0; i < path.size(); i++) {
+    if(path[i].parcel == 1) {
+      sum++;
+    }
+  }
   
   std::cout << "summe: " << sum << '\n';
   return 0;
